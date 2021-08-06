702c80b41d6f8bc933ef8a566b408005729067fd
Facundo Balsells-Miró <facumiro@MacBook-Pro.local>
2021-08-06 14:15:24 -0300

    homework JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -231,7 +231,7 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra.length > 1) {
     return "Dato incorrecto";
-  } else if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+  } else if (letra === "a" || "e" || "i" || "o" || "u") {
     return "Es vocal";
   }
 }
